7f3e15afa44abd5963f5f952adb7866b4f892184
veduram <vedaradhak@gmail.com>
2023-01-25 23:36:21 -0500

    fixed bugs in slew function

--- src/subsystems/chassis.cpp
+++ src/subsystems/chassis.cpp
@@ -186,8 +186,8 @@ void tank_drive() {
   if(abs(left_stick) < 10) left_stick = 0;
   if(abs(right_stick) < 10) right_stick = 0;
 
-  lcd::set_text(4, "left: " + std::to_string(left_stick));
-  lcd::set_text(5, "right: " + std::to_string(right_stick));
+  // lcd::set_text(4, "left: " + std::to_string(left_stick));
+  // lcd::set_text(5, "right: " + std::to_string(right_stick));
 
   // set_chassis(left_stick, right_stick);
   tank_left = left_stick;
@@ -204,10 +204,15 @@ void slew_power() {
   } else if (chassis_mode == DRIVE_TANK) {
     left_in = tank_left;
     right_in = tank_right;
+    max_power = 67;
   } else if (chassis_mode == DRIVE_BRAKE) {
     left_in = 0;
     right_in = 0;
+    max_power = 127;
   }
+
+  lcd::set_text(4, "left: " + std::to_string(left_in));
+  lcd::set_text(5, "right: " + std::to_string(right_in));
 
   //left side
   int v_dir_left = last_left != 0 ? sgn(last_left) : sgn(left_in);
@@ -218,19 +223,22 @@ void slew_power() {
 
   slewed_left = last_left + delta_left;
   if (fabs(slewed_left) > max_power) 
-    slewed_left = v_dir_left * max_power;
+    slewed_left = sgn(slewed_left) * max_power;
 
   //right side
   int v_dir_right = last_right != 0 ? sgn(last_right) : sgn(right_in);
   int a_dir_right = last_right != right_in ? v_dir_right * sgn(right_in - last_right) : 0;
 
   int right_limit = a_dir_right >= 0 ? MAX_ACCEL_SLEW : MAX_DECEL_SLEW;
-  double delta_right = v_dir_right *  (fabs(right_in - last_right) < right_limit ? fabs(right_in - last_right) : right_limit);
+  double delta_right = a_dir_right * v_dir_right * (fabs(right_in - last_right) < right_limit ? fabs(right_in - last_right) : right_limit);
 
   slewed_right = last_right + delta_right;
+
+  lcd::set_text(6, "sleft: " + std::to_string(slewed_right));
+  lcd::set_text(7, "sright: " + std::to_string(slewed_left));
+
   if (fabs(slewed_right) > max_power)
-    slewed_right = v_dir_right * max_power;
-  
+    slewed_right = sgn(slewed_right) * max_power;
 }
 
 int sgn(double num) {
@@ -292,11 +300,14 @@ void chassis_auton() {
 
 void chassis_task(void *parameter) {
   int time = 0;
+
   while (true) {
     odom_update_aps();
 
     if (!competition::is_autonomous()) {
+      chassis_mode = DRIVE_TANK; //comment out later
       tank_drive();
+      // set_chassis(tank_left, tank_right);
     } else {
       //odom_update();
       chassis_auton();
