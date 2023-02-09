d879d1f1edeb727ef876f6ca8d52867351c9f524
YashiDev556 <yashidev556@gmail.com>
2023-02-08 23:10:38 -0500

    faster indexing

--- include/subsystems/globals.hpp
+++ include/subsystems/globals.hpp
@@ -43,7 +43,7 @@
 #define MIN_TRANS_ERROR 0.05
 
 //Flywheel constants
-#define FLYWHEEL_POWER_HIGH 110
+#define FLYWHEEL_POWER_HIGH 100
 #define FLYWHEEL_POWER_LOW 95
 #define FLYWHEEL_GAIN 0.5
 #define INDEXER_POWER 95
@@ -57,7 +57,7 @@
 //need to extern the motors for the front and the back flywheels
 
 //defining the power and the ports for the intake
-#define INTAKE_POWER 100
+#define INTAKE_POWER 105
 #define OUTTAKE_POWER -97
 #define ROLLER_POWER 127
 #define ROLLER_POS -1400
@@ -67,7 +67,7 @@
 
 extern ADIDigitalOut indexer;
 #define INDEXER_PORT 'G'
-#define INDEXER_ENGAGE_COOLDOWN 400
+#define INDEXER_ENGAGE_COOLDOWN 150
 #define INDEXER_ENGAGE_TIME 100
 
 
@@ -100,7 +100,7 @@ extern ADIEncoder odom_right;
 //defining the V5 ports on the brain
 #define DRIVE_MOTOR_PORT_BL 5
 #define DRIVE_MOTOR_PORT_BR 6
-#define DRIVE_MOTOR_PORT_FL 3
+#define DRIVE_MOTOR_PORT_FL 1
 #define DRIVE_MOTOR_PORT_FR 4
 
 
