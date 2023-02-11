7ac259f68b1a7d02dfffde6388e023baea6d35ee
veduram <vedaradhak@gmail.com>
2023-02-10 22:48:19 -0500

    left auton tuning

--- src/subsystems/routines.cpp
+++ src/subsystems/routines.cpp
@@ -24,7 +24,7 @@ void left_roller() {
 
 void left_roller_and_disc() {
   straight_pid(6, 30);
-  set_flywheel(120);//MAGIC NUM
+  set_flywheel(121);//MAGIC NUM
   delay(100);
   set_intake(-127);
   delay(110);
@@ -33,29 +33,33 @@ void left_roller_and_disc() {
   straight_pid(-5, 80);
   delay(1200);
 
-  turn_pid(-20, 80);
+  turn_pid(-18, 80);
+  delay(1000);
   delay(1500);
 
   // turn_pid(35, 80);
   // delay(2000);
 
   engage_indexer();
-  delay(2000);
+  set_flywheel(121);//MAGIC NUM
+  delay(1000);
+
+  delay(2300);
   engage_indexer();
   delay(200);
   set_flywheel(LOW);//MAGIC NUM
-  turn_pid(-102, 80);
+  turn_pid(-110, 80);
   delay(1400);
-  straight_pid(30, 127);
+  straight_pid(28, 127);
 
   delay(1000);
 
   set_intake(INTAKE_POWER);
   straight_pid(20, 35);
-  delay(2600);
-  set_flywheel(120);
-  turn_pid(75, 80);
-  set_flywheel(116);
+  delay(2300);
+  set_flywheel(121);
+  turn_pid(85, 80);
+  set_flywheel(127);
   delay(2100);
   set_intake(0);
   engage_indexer();
@@ -69,7 +73,7 @@ void left_roller_and_disc() {
 void right_roller_and_disc() {
   
   // toggle_intake();
-  set_intake(INTAKE_POWER);
+  set_intake(INTAKE_POWER + 5);
   straight_pid(30, 65);
   delay(1000);
   delay(1200);
@@ -86,13 +90,13 @@ void right_roller_and_disc() {
   // toggle_intake();
   set_intake(0);
 
-  delay(3400);
+  delay(3100);
   engage_indexer();
   set_flywheel(117);
 
-  delay(2100);
+  delay(1500);
   engage_indexer();
-  delay(2100);
+  delay(1500);
   // straight_pid(2, 60);
   engage_indexer();
   delay(200);
@@ -109,7 +113,7 @@ void right_roller_and_disc() {
   delay(600);
   // intake.move_relative(-300, ROLLER_POWER);
   set_intake(-127);
-  delay(100);
+  delay(130);
   set_intake(0);
   straight_pid(-2, 70);
 }
